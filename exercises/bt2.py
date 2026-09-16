c06450a5996062f806b3ced759b7d72269efa71f
Phú Lê <phule@Phus-MacBook-Air.local>
2026-09-16 19:30:40 +0700

    day 1 exercises

---
+++ exercises/bt2.py
@@ -0,0 +1,5 @@
+Revenue = 100000000
+Cost = 65000000
+print("Revenue: ", f'{Revenue: ,} VND', "\nCost: ", f'{Cost: ,} VND')
+print("Profit", f'{Revenue - Cost: ,} VND')
+print("Profit Margin", ((Revenue - Cost) / Revenue)*100)
---
+++ exercises/day1_hello.py
@@ -0,0 +1,5 @@
+name = "Your Name"
+goal = "AI Engineer"
+
+print("Hello,", name)
+print("My goal is to become an", goal)
